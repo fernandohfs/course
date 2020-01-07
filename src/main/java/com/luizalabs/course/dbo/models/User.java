8ec83c9e35c878aac24ab6ce1542ecfae2785801
Fernando Soares <fernando.soares@luizalabs.com>
2020-01-07 17:24:10 -0300

    H2 database, test profile, JPA

--- src/main/java/com/luizalabs/course/dbo/models/User.java
+++ src/main/java/com/luizalabs/course/dbo/models/User.java
@@ -6,6 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
 import java.io.Serializable;
 
 @NoArgsConstructor
@@ -13,10 +18,14 @@ import java.io.Serializable;
 @EqualsAndHashCode(of = "id")
 @Data
 @Builder
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String email;
