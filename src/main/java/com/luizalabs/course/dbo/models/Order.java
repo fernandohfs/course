b34637ed564931cae7120128d22cce272cad0c0e
Fernando Soares <fernando.soares@luizalabs.com>
2020-01-08 14:18:28 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/luizalabs/course/dbo/models/Order.java
+++ src/main/java/com/luizalabs/course/dbo/models/Order.java
@@ -5,18 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Setter;
-import lombok.AccessLevel;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.GenerationType;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,21 +22,18 @@ import java.util.List;
 @Data
 @Builder
 @Entity
-@Table(name = "users")
-public class User implements Serializable {
+@Table(name = "orders")
+public class Order implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  private String name;
-  private String email;
-  private String phone;
-  private String password;
+  private Instant moment;
 
-  @Setter(AccessLevel.NONE)
-  @OneToMany(mappedBy = "client")
-  private List<Order> orders = new ArrayList<>();
+  @ManyToOne
+  @JoinColumn(name = "client_id")
+  private User client;
 
 }
